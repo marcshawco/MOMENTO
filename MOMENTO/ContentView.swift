57fba417e0ac42f1452657429ccc1b789cb74e4f
Marc Shaw <marcus@marcuslshaw.com>
2026-02-21 15:46:12 -0800

    Initial Commit

---
+++ MOMENTO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MOMENTO
+//
+//  Created by Marcus Shaw II on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MOMENTO/MOMENTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOMENTOApp.swift
+//  MOMENTO
+//
+//  Created by Marcus Shaw II on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MOMENTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
